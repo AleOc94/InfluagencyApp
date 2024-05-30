9adf13097e6c62c8ebf6445c34bf0efeef631991
enriqueemoreno8 <ema0017@alu.medac.es>
2024-05-30 13:44:09 +0200

    Facebook

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'registro_correo.dart'; // Importa la nueva pantalla
-import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'user_screen.dart'; // Importamos el widget UserScreen
+import 'registro_correo.dart'; // Importamos el widget RegistroCorreoScreen
+import 'package:hive_flutter/hive_flutter.dart'; // Import Hive Flutter
+import 'package:awesome_notifications/awesome_notifications.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    print('Firebase initialized successfully.'); // Mensaje de depuración
-  } catch (error) {
-    print('Error initializing Firebase: $error'); // Mensaje de depuración
-  }
-
+  await Firebase.initializeApp();
+  await Hive.initFlutter(); // Initialize Hive
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});
@@ -27,8 +22,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Influagency',
       theme: ThemeData(
-        primaryColor: const Color.fromARGB(255, 133, 25, 240), // Morado
-        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color.fromARGB(255, 255, 214, 90)), // Amarillo
+        primaryColor: const Color.fromARGB(255, 133, 25, 240),
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color.fromARGB(255, 255, 214, 90)),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MainScreen(),
@@ -39,75 +34,55 @@ class MyApp extends StatelessWidget {
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key});
 
+  Future<void> _signInWithFacebook(BuildContext context) async {
+    print('Starting Facebook sign in...'); // Debug print
+    try {
+      final LoginResult result = await FacebookAuth.instance.login();
+      print('LoginResult: ${result.status}'); // Debug print
+      if (result.status == LoginStatus.success) {
+        final AccessToken? accessToken = result.accessToken;
+        print('AccessToken: ${accessToken?.tokenString}'); // Debug print
+        if (accessToken != null) {
+          final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(accessToken.tokenString);
+          await FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+          print('Firebase sign in successful'); // Debug print
+          // Navigate to UserScreen after successful login
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const UserScreen(),
+            ),
+          );
+        } else {
+          print('Access token is null');
+        }
+      } else {
+        print('Facebook sign in failed: ${result.message}');
+      }
+    } catch (e) {
+      print('Error during Facebook sign in: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 133, 25, 240), // Fondo morado
+      backgroundColor: const Color.fromARGB(255, 133, 25, 240),
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset(
-                'assets/images/influagency_logo.png', // Asegúrate de tener esta imagen en tu carpeta de assets
-                height: 200,
-              ),
+              Image.asset('assets/images/influagency_logo.png', height: 200),
               const SizedBox(height: 50),
-              const Text(
-                'Al tocar en entrar, usted acepta nuestros Términos. Sepa cómo procesamos sus datos en nuestra Política de Privacidad y Política de cookies.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              RichText(
-                textAlign: TextAlign.center,
-                text: const TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Términos',
-                      style: TextStyle(
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                    TextSpan(
-                      text: '. Sepa cómo procesamos sus datos en nuestra ',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    TextSpan(
-                      text: 'Política de Privacidad',
-                      style: TextStyle(
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                    TextSpan(
-                      text: ' y ',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    TextSpan(
-                      text: 'Política de cookies',
-                      style: TextStyle(
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                    TextSpan(
-                      text: '.',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 30),
+              // Texts and Buttons...
               ButtonWidget(
                 imagePath: 'assets/images/google_icon.png',
                 text: 'ENTRAR CON GOOGLE    ',
                 onPressed: () {
-                  // Acción para el botón de Google
+                  // Action for the Google button
+                  print('Google button pressed'); // Debug print
                 },
               ),
               const SizedBox(height: 20),
@@ -115,9 +90,11 @@ class MainScreen extends StatelessWidget {
                 imagePath: 'assets/images/correo_icon.png',
                 text: 'ENTRAR CON E-MAIL      ',
                 onPressed: () {
+                  print('Email button pressed'); // Debug print
+                  // Navigate to the email registration screen
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => RegistroCorreoScreen()), // Navega a la nueva pantalla
+                    MaterialPageRoute(builder: (context) => RegistroCorreoScreen()),
                   );
                 },
               ),
@@ -125,9 +102,7 @@ class MainScreen extends StatelessWidget {
               ButtonWidget(
                 imagePath: 'assets/images/facebook_icon.png',
                 text: 'ENTRAR CON FACEBOOK',
-                onPressed: () {
-                  // Acción para el botón de Facebook
-                },
+                onPressed: () => _signInWithFacebook(context),
               ),
             ],
           ),
@@ -149,7 +124,7 @@ class ButtonWidget extends StatelessWidget {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromARGB(255, 255, 214, 90), // Fondo amarillo
+        backgroundColor: const Color.fromARGB(255, 255, 214, 90),
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8),
@@ -161,7 +136,6 @@ class ButtonWidget extends StatelessWidget {
           Image.asset(
             imagePath,
             height: 24,
-           
           ),
           const SizedBox(width: 10),
           Text(
